38fc40e03582b4c4c8f2ff30b777468c1d29dbc1
James Lomax <james@citymapper.com>
2021-12-02 19:01:54 +0000

    Day 2 part 1

---
+++ day02/src/main.rs
@@ -0,0 +1,54 @@
+use vector2d::Vector2D;
+
+type Vec2i = Vector2D<i32>;
+
+
+fn instruction_to_vec(line: &str) -> Result<Vec2i, String> {
+    let mut split = line.split(" ");
+    let direction = split.next().ok_or("Missing direction field".to_string())?;
+    let distance = split.next().ok_or("Missing distance field".to_string())?;
+    let distance = distance.parse::<i32>().ok().ok_or(format!("Invalid distance '{}' (must be integer)", distance))?;
+    match direction {
+        "forward" => Ok(Vec2i::new(distance, 0)),
+        "down" => Ok(Vec2i::new(0, distance)),
+        "up" => Ok(Vec2i::new(0, -distance)),
+        _ => Err(format!("Unexpected direction '{}'", direction))
+    }
+}
+
+fn run_program(program_str: &str) -> i32 {
+    let pos: Vec2i = program_str.split("\n")
+        .map(|s| s.trim()).filter(|s| s.len() > 0)
+        .map(|l| instruction_to_vec(l).unwrap())
+        .fold(Vec2i::new(0, 0), |acc, x| acc + x);
+    pos.x * pos.y
+}
+
+fn main() {
+    let contents = std::fs::read_to_string("input.txt").expect("file error");
+    println!("Part 1 = {}", run_program(&contents));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_instruction_parse() {
+        assert_eq!(Ok(Vec2i::new(2, 0)), instruction_to_vec("forward 2"));
+        assert_eq!(Ok(Vec2i::new(0, 2)), instruction_to_vec("down 2"));
+        assert_eq!(Ok(Vec2i::new(0, -3)), instruction_to_vec("up 3"));
+        assert_eq!(Err("Unexpected direction 'left'".to_string()), instruction_to_vec("left 5"));
+    }
+
+    #[test]
+    fn test_program() {
+        let sample = "forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2";
+        assert_eq!(150, run_program(sample));
+    }
+}
