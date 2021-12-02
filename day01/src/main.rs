6bf13cc0e6a60fdd9ae6a808556fb7a56be06afd
James Lomax <james@citymapper.com>
2021-12-02 18:11:39 +0000

    Reimagining count_increases function with iterators for the hell of it

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,23 +7,24 @@ fn parse_depths(input: &str) -> Vec<u32> {
         .collect()
 }
 
-fn count_increases(depths: &Vec<u32>) -> usize {
-    zip(depths, &depths[1..])
+fn count_increases<I>(depths: I) -> usize 
+    where I: Iterator<Item = u32> + Clone
+{
+    zip(depths.clone(), depths.skip(1))
         .filter(|(a, b)| a < b)
         .count()
 }
 
 fn count_window_increases(depths: &Vec<u32>) -> usize {
-    let windepths = zip(zip(depths, &depths[1..]), &depths[2..])
-        .map(|((a, b), c)| a + b + c)
-        .collect();
-    count_increases(&windepths)
+    count_increases(
+        zip(zip(depths, &depths[1..]), &depths[2..])
+            .map(|((a, b), c)| a + b + c))
 }
 
 fn main() {
     let contents = std::fs::read_to_string("input.txt").expect("file error");
     let depths = parse_depths(&contents);
-    println!("Part 1 = {}", count_increases(&depths));
+    println!("Part 1 = {}", count_increases(depths.iter().cloned()));
     println!("Part 2 = {}", count_window_increases(&depths));
 }
 
@@ -44,7 +45,7 @@ mod tests {
             260
             263";
         let depths = parse_depths(sample);
-        assert_eq!(7, count_increases(&depths));
+        assert_eq!(7, count_increases(depths.iter().cloned()));
         assert_eq!(5, count_window_increases(&depths));
     }
 }
